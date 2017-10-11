0fe93b61abdb3a172d3c8dadce130ad7696aa4b1
Penghui Liao <liaoishere@gmail.com>
2017-10-12 00:24:27 +0800

    2017/10/12
    Signed-off-by: Penghui Liao <liaoishere@gmail.com>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,6 +21,10 @@ source ~/.config/fish/xsky.fish
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
+# pyenv virtualenv
+# status --is-interactive; and . (pyenv init -|psub)
+# status --is-interactive; and . (pyenv virtualenv-init -|psub)
+
 # Cowsay
 # python -c "import sys, random; stdout = sys.stdout; sys.stdout = type('BlackHole', (), {'write': (lambda self, string: '')})(); import this; sys.stdout = stdout; print random.choice(''.join([this.d.get(i, i) for i in this.s]).splitlines()[2:]);" | cowsay -f www | lolcat
 # fortune -n 160 50% computers 50% linuxcookie | cowsay -s -W 50 -f www | lolcat
