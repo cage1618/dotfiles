6f47405f83157cd1cb3ea792a2fa8080c9aac976
Penghui Liao <liaoishere@gmail.com>
2018-04-25 10:37:13 +0800

    update on 2018/04/25
    Signed-off-by: Penghui Liao <liaoishere@gmail.com>

--- fish/xsky.fish
+++ fish/xsky.fish
@@ -14,6 +14,11 @@ function fetch-xsky
     git fetch -p xsky; and git fetch --tags xsky
 end
 
+function merge-xsky
+    set branch (git rev-parse --abbrev-ref HEAD | egrep -v '^HEAD$'; or git rev-parse HEAD)
+    git merge xsky/$branch
+end
+
 function del-br
     git br -D $argv
     git push origin :$argv
