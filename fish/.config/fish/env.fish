b324c84d88586241d75c975dbe288b8d0634cb12
Penghui Liao <liaoishere@gmail.com>
2018-11-20 17:48:00 +0800

    update 2018/11/20
    Signed-off-by: Penghui Liao <liaoishere@gmail.com>

--- fish/.config/fish/env.fish
+++ fish/.config/fish/env.fish
@@ -26,6 +26,7 @@ set -gx GPG_TTY (tty)
 # FZF
 set -gx FZF_DEFAULT_COMMAND "fd --type f -E 'vendor/' -E 'bundles/' --no-ignore-vcs -I"
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set -gx FZF_DEFAULT_OPTS '--height 50% --layout=reverse --border'
 
 # alias
 alias v='vim'
